f5ade2cc4f04c2ee00c64df4819bb4a74e38f9c5
balintovics <molnar99b@gmail.com>
2021-05-22 21:16:30 +0200

    feat: skeleton jinja template created, css & js files created, fixed textarea textcontent to value

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template
 
-
-app = Flask(__name__)
+app = Flask("amigo")
 
 
 @app.route('/')
@@ -9,8 +8,7 @@ def speech():
     return render_template('speech.html')
 
 
-
 if __name__ == "__main__":
     app.run(
-        debug=True
-    )
+        debug=True,
+        port=8000)
