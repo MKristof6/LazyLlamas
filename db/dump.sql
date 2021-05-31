3e6033eacb4acb07917e0bfdf9748d86b0faff00
balintovics <molnar99b@gmail.com>
2021-05-31 19:40:12 +0200

    feat: insert queries and HTML templates for registration & handling it on server side

--- db/dump.sql
+++ db/dump.sql
@@ -1,2 +1,9 @@
-INSERT INTO teacher (name, email, password) VALUES ('balintovics', 'molnar99b@gmail.com', '$2b$12$2K/wNJVJlW85R2XpzJmmsOa4dKvHjS3.gFU4079avMp5QQnsJTRMe');
-INSERT INTO student (name, email, password, birthday) VALUES ('Barna', 'barna.urmossy@gmail.com', '$2b$12$GEgi6u3iUyUQU0QusefE1.6MeMPhJzLrhOhovID.IHKukCfW91T1i', '1940.02.29.');
+-- INSERT INTO teacher (name, email, password) VALUES ('balintovics', 'molnar99b@gmail.com', '$2b$12$N/XIozKGAVxNZGqDpa.IW.pi1JYdlXguyTyKmXekvjel.5GC6uRpu');
+INSERT INTO student (name, email, password, birthday) VALUES ('Zsófi', 'zsofiaszonja.kassai@gmail.com', '$2b$12$N/XIozKGAVxNZGqDpa.IW.pi1JYdlXguyTyKmXekvjel.5GC6uRpu', '1969.06.09.');
+INSERT INTO student (name, email, password, birthday) VALUES ('Kristóf', 'kristof.murai@gmail.com', '$2b$12$N/XIozKGAVxNZGqDpa.IW.pi1JYdlXguyTyKmXekvjel.5GC6uRpu', '1969.06.09.');
+INSERT INTO student (name, email, password, birthday) VALUES ('Barna', 'barna.urmossy@gmail.com', '$2b$12$N/XIozKGAVxNZGqDpa.IW.pi1JYdlXguyTyKmXekvjel.5GC6uRpu', '1969.06.09.');
+
+INSERT INTO language(name) VALUES ('English');
+INSERT INTO language(name) VALUES ('French');
+INSERT INTO language(name) VALUES ('Italian');
+INSERT INTO language(name) VALUES ('Spanish');
