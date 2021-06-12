2fe58586d7627b092c2ccb43830ac01f72f95d6b
MKristof6 <kristof.murai@gmail.com>
2021-06-12 18:07:53 +0200

    Add languages

--- static/js/listeningGame.js
+++ static/js/listeningGame.js
@@ -1,5 +1,5 @@
 function get_data(question_id) {
-    fetch(`/listening-game/${question_id}`)
+    fetch(`/get-listening-game/${question_id}`)
         .then(response => response.json())
         .then(data => getElements(data))
         .then(error => console.log("Error: " + error))
@@ -7,18 +7,19 @@ function get_data(question_id) {
 
 
 function getElements(data) {
+
+
     elements = ``
 
     for (let element of data) {
-
         // console.log(element);
-        console.log(" ")
+
         elements += `
             <div class="card">
-                <input type="image" id="speak" value="${element['answer']}" src="./static/images/play.png"
+                <input type="image" value="${element['answer']}" name="${element['language']}" src="./static/images/play.png"
                     draggable="false"  class="answer" >
-                <div class="asd">
-                <div class="possibilities">
+                <div class="asd" id="${element['answer']}">
+                <div class="possibilities" >
                     <p>${element["possibilities"][0]}</p>
                    </div>
                     <div class="possibilities">
@@ -39,8 +40,42 @@ function showElements(elements) {
     let container = document.getElementById("container")
     container.innerHTML = elements
     init()
+    setupEventListener()
 
 }
+
+function check(e) {
+    console.log(e.innerText)
+    console.log(e.parentElement.id)
+
+
+    // parentElement id = child element value id
+
+
+    deactivateClass()
+}
+function deactivateClass(){
+
+
+
+
+    setupEventListener()
+}
+
+
+function setupEventListener(){
+    let words = document.querySelectorAll('.possibilities')
+        for( let word of words){
+            word.addEventListener("click", e=>{
+                console.log("ok")
+                check(e.target)
+
+
+            })
+        }
+
+}
+
 
 
 function init() {
@@ -48,7 +83,9 @@ function init() {
     for (let button of buttons) {
         button.addEventListener("click", e => {
             let answer = e.target.value
-            responsiveVoice.speak(answer);
+            let language = e.target.name
+            responsiveVoice.speak(answer, language);
+            console.log(language)
         });
     }
 }
