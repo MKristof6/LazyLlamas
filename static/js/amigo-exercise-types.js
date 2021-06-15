b91ccfdd3c9bf67112c9355d9164b5f03672dd10
zsofikassai <zsofiaszonja.kassai@gmail.com>
2021-06-15 19:14:59 +0200

    refactor: user inputs required on upload

--- static/js/amigo-exercise-types.js
+++ static/js/amigo-exercise-types.js
@@ -1,14 +1,8 @@
 let GameBtns = document.querySelectorAll('.exercise-type');
 GameBtns.forEach(item =>{item.addEventListener('click', redirect);})
 
-let sendBtns = document.querySelectorAll('.send');
-sendBtns.forEach(item =>{item.addEventListener('click', sendTask)})
-
 function redirect() {
   window.location.href = `/${this.id}`;
 
 }
 
-function sendTask(){
-    console.log(this.id);
-}
--- static/js/comprehensiveUpload.js
+++ static/js/comprehensiveUpload.js
@@ -1,33 +1,35 @@
 import {networkHandler} from "./networkHandler.js";
 
-let DATA = {};
-const questionAdder = document.getElementById('button');
-const submit = document.getElementById('submit');
+const addQuestionBtn = document.getElementById('button');
 
-questionAdder.addEventListener('click', newQuestion);
-submit.addEventListener('click', collectAndSend);
+let comprehensiveReadingUpload = {
+    init: function () {
+        const submit = document.getElementById('submit');
+        addQuestionBtn.addEventListener('click', this.addQuestion);
+        submit.addEventListener('click', this.collectAndSendData);
+    },
 
+    collectAndSendData: function () {
+        let data = {};
+        data['language'] = document.getElementById("language").value;
+        data['theme'] = document.getElementById('theme').value;
+        data['long-text'] = document.getElementById('long-text').value;
+        let questionInputs = document.querySelectorAll('.questions');
+        let questions = [];
+        for (let question of questionInputs) {
+            if (question.value !== 'Itt tehetsz fel újabb kérdést') questions.push(question.value);
+        }
+        data['questions'] = questions;
+        networkHandler.sendData(data, '/comprehensive-reading-upload', networkHandler.redirectHome);
 
-function collectAndSend() {
-    DATA['language'] = document.getElementById("language").value;
-    DATA['theme'] = document.getElementById('theme').value;
-    DATA['long-text'] = document.getElementById('long-text').value;
-    const questionInputs = document.querySelectorAll('.questions'); //Question's number are dependent on the user, so looping through them by class
-    let questions = [];
-    for (let question of questionInputs) {
-        if (question.value !== 'Itt tehetsz fel újabb kérdést') questions.push(question.value);
+    },
+
+    addQuestion: function () {
+        let additionalQuestion = `<input type="text" class="questions" value="${addQuestionBtn.previousElementSibling.value}">`;
+        document.querySelector('.question-container').firstElementChild.insertAdjacentHTML('beforeend', additionalQuestion);
+        addQuestionBtn.previousElementSibling.value = '';
     }
-    DATA['questions'] = questions;
-    networkHandler.sendData(DATA, '/comprehensive-reading-upload', networkHandler.redirectHome);
 }
 
-
-
-
-function newQuestion() {
-    let additionalQuestion = `<input type="text" class="questions" value="${questionAdder.previousElementSibling.value}">`;
-    document.querySelector('.question-container').firstElementChild.insertAdjacentHTML('beforeend', additionalQuestion);
-    questionAdder.previousElementSibling.value = '';
-}
-
+comprehensiveReadingUpload.init();
 
