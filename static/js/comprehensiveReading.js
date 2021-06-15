65cf8c26d3d78cd630567dd3a96b94487bea09db
zsofikassai <zsofiaszonja.kassai@gmail.com>
2021-06-15 22:24:53 +0200

    refactor: comprehensiveReading.js

--- static/js/comprehensiveReading.js
+++ static/js/comprehensiveReading.js
@@ -1,28 +1,21 @@
-let gameId = document.querySelector(".comprehensive-reading").id;
+import {networkHandler} from "./networkHandler.js";
+let comprehensiveReading = {
+    init: function () {
+        let gameId = document.querySelector(".comprehensive-reading").id;
+        let saveBtn = document.getElementById('save');
+        let solution = [];
+        let answers = [];
 
-let saveBtn = document.getElementById('save');
-
-
-let solution = [];
-let answers = [];
-saveBtn.addEventListener('click', () => {
-    answers = document.querySelectorAll('.answer');
-    for (let answer of answers) {
-        solution.push(answer.value);
-    }
-    comprehensiveReading.sendData(solution);
-})
-
-
-
-const comprehensiveReading = {
-    getData: function (route, callback) {
-        fetch(route)
-            .then(response => response.json())
-            .then(data => this.showData(data))
+        saveBtn.addEventListener('click', () => {
+            answers = document.querySelectorAll('.answer');
+            for (let answer of answers) {
+                solution.push(answer.value);
+            }
+            networkHandler.sendData(solution, `/comprehensive-reading-solution-saver/${gameId}`, networkHandler.redirectHome);
+        })
+        networkHandler.getData(`/get-comprehensive-reading/${gameId}`, comprehensiveReading.showData);
 
     },
-
     showData: function (data) {
         let readingContainer = document.querySelector('.reading-container');
         let questionContainer = document.querySelector('.question-container');
@@ -37,23 +30,6 @@ const comprehensiveReading = {
         readingContainer.innerHTML = data['long_text'];
         questionContainer.innerHTML = questionList;
 
-    },
-
-    sendData: function (solution) {
-    fetch(`/comprehensive-reading-solution-saver/${gameId}`, {
-        method: "POST",
-        body: JSON.stringify(solution),
-        headers: {"Content-type": "application/json; charset=UTF-8"}
-    })
-        .then(response => response.json())
-        .then(json => console.log(json))
+    }
 }
-
-}
-
-
-function init() {
-    comprehensiveReading.getData(`/get-comprehensive-reading/${gameId}`, comprehensiveReading.showData);
-}
-
 init();
