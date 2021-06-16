6a1296a0ddced039ac97d7a69a91be04dd1bbc26
zsofikassai <zsofiaszonja.kassai@gmail.com>
2021-06-16 02:14:27 +0200

    refactor

--- static/js/exercise-types.js
+++ static/js/exercise-types.js
@@ -1,8 +1,6 @@
 let GameBtns = document.querySelectorAll('.exercise-type');
 GameBtns.forEach(item =>{item.addEventListener('click', redirect);})
 
-
-
 function redirect() {
   window.location.href = `/${this.id}`;
 
--- static/js/fillingGame.js
+++ static/js/fillingGame.js
@@ -1,27 +1,23 @@
-const gameId = document.querySelector(".filling-gaps").id;
-let solution = [];
-const saveBtn = document.getElementById('save');
-
-saveBtn.addEventListener('click', () => {
-    const answers = document.querySelectorAll('.gap');
-    answers.forEach(answer => solution.push(answer.value));
-    console.log(solution);
-    fillingGame.sendData(solution);
-})
+import {networkHandler} from "./networkHandler.js";
 
 const fillingGame = {
-    getData: function (route, callback) {
-        fetch(route)
-            .then(response => response.json())
-            .then(data => this.showData(data))
+    init: function () {
+        const gameId = document.querySelector(".filling-gaps").id;
+        let solution = [];
+        const saveBtn = document.getElementById('save');
+
+        saveBtn.addEventListener('click', () => {
+            const answers = document.querySelectorAll('.gap');
+            answers.forEach(answer => solution.push(answer.value));
+            networkHandler.sendData(solution, `/filling-gap-solution-saver/${gameId}`, networkHandler.redirectHome);
+        })
+
+        networkHandler.getData(`/get-filling-game/${gameId}`, fillingGame.showData);
 
     },
-
     showData: function (data) {
-        console.log(data);
         let readingContainer = document.querySelector('.filling-container');
         document.getElementById('theme').textContent = data['theme'];
-
         const textParts = data['long_text'];
         textParts.forEach(part => part.replaceAll('.', '.\n'));
         let content = '';
@@ -30,25 +26,9 @@ const fillingGame = {
             if (i !== textParts.length - 1) content += `<input type="text" class="gap">`;
         }
         readingContainer.innerHTML = content;
-    },
-
-    sendData: function (solution) {
-        fetch(`/filling-gap-solution-saver/${gameId}`, {
-            method: "POST",
-            body: JSON.stringify(solution),
-            headers: {"Content-type": "application/json; charset=UTF-8"}
-        })
-            .then(response => response.json())
-            .then(json => console.log(json))
-            .then(r => {
-                if (r.status.code === 200) window.loaction = '/';
-        })
     }
+
 }
 
 
-function init() {
-    fillingGame.getData(`/get-filling-game/${gameId}`, fillingGame.showData);
-}
-
-init();
+fillingGame.init();
